6ebd280e3dfc1da80a1c8308c35c5b3eed6ba572
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-18 01:30:12 -0300

    Fetch data from the local server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,9 @@ function App() {
       <h1>Hello Go, go bring me data: </h1>
       <button
         onClick={() => {
-          fetch("http://localhost:3000/users").then(data => data.json());
+          fetch("http://localhost:3000/users")
+            .then(res => res.json())
+            .then(data => console.log(data));
         }}
       >
         Get data

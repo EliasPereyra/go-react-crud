22bf5ad3d665c459f616b6fce705eafce3bc72dd
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-19 22:16:43 -0300

    Connect to mongodb

--- main.go
+++ main.go
@@ -1,22 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	app := fiber.New()
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
 
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		port = "4000"
 	}
+	
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable.")
+	}
 
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+	
 	app.Use(cors.New())
 
 	app.Static("/", "./client/dist")
@@ -28,5 +47,4 @@ func main() {
 	})
 
 	app.Listen(":" + port)
-	fmt.Println("Server on port 3000")
 }

7365adfb198a7df771f3a56b298e386a358f8898
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-20 18:46:26 -0300

    Import the form component in the main component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,24 @@
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Go, go bring me data: </h1>
-      <button
-        onClick={() => {
-          fetch("http://localhost:3000/users")
-            .then(res => res.json())
-            .then(data => console.log(data));
-        }}
-      >
-        Get data
-      </button>
-      <span>{}</span>
-    </div>
+    <section className="App">
+      <div>
+        <h1>Hello Go, go bring me data: </h1>
+        <button
+          onClick={() => {
+            fetch("/users")
+              .then(res => res.json())
+              .then(data => console.log(data));
+          }}
+        >
+          Get data
+        </button>
+      </div>
+
+      <Form />
+    </section>
   );
 }
 

8bc0205644e3e7bc3ef8803a8fbb60b89bfb3a35
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-01-23 23:33:07 -0300

    Import the users list component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,11 @@
 import "./App.css";
 import Form from "./components/Form";
+import UsersList from "./components/UsersList";
 
 function App() {
   return (
     <section className="App">
-      <div>
-        <h1>Hello Go, go bring me data: </h1>
-        <button
-          onClick={() => {
-            fetch("/users")
-              .then(res => res.json())
-              .then(data => console.log(data));
-          }}
-        >
-          Get data
-        </button>
-      </div>
+      <UsersList />
 
       <Form />
     </section>

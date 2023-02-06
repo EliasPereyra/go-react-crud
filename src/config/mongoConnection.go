b821e420c414d826ba85c8bbd1c6aebfb88184ab
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-02-05 23:28:57 -0300

    Create a separate file for connecting to Mongodb

---
+++ src/config/mongoConnection.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func connectMongoDB() *mongo.Client {
+	cfg := getConfig()
+
+	if cfg.MONGODB_URI == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable.")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(cfg.MONGODB_URI))
+	if err != nil {
+		panic(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+
+	return client
+}
+
+var DB *mongo.Client = connectMongoDB()
+
+func getCollections(client *mongo.Client) *mongo.Collection {
+	collection := client.Database("go-mongo").Collection("users")
+
+	return collection
+}

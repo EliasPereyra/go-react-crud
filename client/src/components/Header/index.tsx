05eda7d6f020bbc58daabf98717e1642456329ea
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-02-10 19:01:37 -0300

    Create a header component

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function Header() {
+  return (
+    <div>
+      <nav>
+        <h3>Welcome to Task assigner</h3>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;

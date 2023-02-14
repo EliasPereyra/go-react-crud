0450eb15bed82c293411526a36411e523a42a0d2
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-02-13 23:27:11 -0300

    Import the header component in the main component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import Form from "./components/Form";
+import Header from "./components/Header";
 import UsersList from "./components/UsersList";
 
 function App() {
   return (
-    <section className="App">
-      <UsersList />
-
-      <Form />
-    </section>
+    <>
+      <Header />
+      <section className="App">
+        <UsersList />
+        <Form />
+      </section>
+    </>
   );
 }
 

d5f100e26dedf2839c3482dc1480453a05f97d6f
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-11-04 23:21:11 -0300

    Feat: add a new img field for the note model

--- src/models/note_model.go
+++ src/models/note_model.go
@@ -3,11 +3,12 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Note struct {
-	_id					primitive.ObjectID	`bson:"_id"`
-	Title				string							`json:"name" form:"name"`
-	Description	string							`json:"description" form:"description"`
-	Created_at	primitive.DateTime	`json:"created_at" form:"created_at"`
-	Updated_at	primitive.DateTime	`json:"updated_at" form:"updated_at"`
-	User_id			string							`json:"user_id" form:"user_id"`
-	Category_id	int									`json:"category_id" form:"category_id"`
+	_id         primitive.ObjectID `bson:"_id"`
+	Title       string             `json:"name" form:"name"`
+	Description string             `json:"description" form:"description"`
+	Img_url     string             `json:"img_url" form:"img_url"`
+	Created_at  primitive.DateTime `json:"created_at" form:"created_at"`
+	Updated_at  primitive.DateTime `json:"updated_at" form:"updated_at"`
+	User_id     string             `json:"user_id" form:"user_id"`
+	Category_id int                `json:"category_id" form:"category_id"`
 }
